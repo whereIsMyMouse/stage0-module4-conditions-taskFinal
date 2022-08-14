8083df0441e9d1eec1e6c93f24e2774d31a1daee
shariy <sharii.margarita@gmail.com>
2022-08-14 03:12:32 +0300

    commit1

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String message = "";
+        message = (x == 0 && y == 0) ? "zero" :
+                (message = (x > 0 && y > 0) ? "first" :
+                        (message = (x < 0 && y > 0) ? "second" :
+                                (message = (x < 0 && y < 0) ? "third" : "fourth")));
+        System.out.println(message);
     }
 }
+
+
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,32 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month <= 0 || month > 12) {
+            System.out.println("invalid date");
+        } else {
+            switch (month){
+                case 2:
+                    if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+                        System.out.println("29");
+                    } else {
+                        System.out.println("28");
+                    }
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println("31");
+                    break;
+                default:
+                    System.out.println("30");
+                    break;
 
+            }
+        }
     }
 }
+
